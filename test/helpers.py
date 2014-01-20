76692b7fe8b31b33b379b76fe8ea493248401150
Dan Carley <dan.carley@gmail.com>
2014-01-20 21:31:00 +0000

    CollectdConfig helper
    Cheap mock of collectd's `Config` class. Takes a nested three item tuple.
    Can't take a dictionary because nodes can have values *and* children, plus
    key names need to not be unique. This is about the simplest thing without
    having to write/modify an XML parser.

--- test/helpers.py
+++ test/helpers.py
@@ -1,2 +1,14 @@
 from nose.tools import *
 from mock import MagicMock, patch
+
+class CollectdConfig(object):
+    """
+    Quacks like collectd Config object.
+    """
+    def __init__(self, key, vals, children):
+        self.key = key
+        self.values = (vals,)
+        self.children = [
+            CollectdConfig(k,v,c)
+            for k,v,c in children
+        ]
